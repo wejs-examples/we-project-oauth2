c6f432f7be52d6f928583a67e97518a819be3b92
albertosouza <alberto.souza.99@gmail.com>
2016-06-01 03:04:31 -0300

    add config/roles.js

---
+++ config/roles.js
@@ -0,0 +1,23 @@
+module.exports = {
+'roles': {
+	'administrator': {
+		'name': 'administrator',
+		'permissions': []
+	},
+	'authenticated': {
+		'name': 'authenticated',
+		'permissions': [],
+		'isSystemRole': true
+	},
+	'unAuthenticated': {
+		'name': 'unAuthenticated',
+		'permissions': [],
+		'isSystemRole': true
+	},
+	'owner': {
+		'name': 'owner',
+		'permissions': [],
+		'isSystemRole': true
+	}
+}
+};
